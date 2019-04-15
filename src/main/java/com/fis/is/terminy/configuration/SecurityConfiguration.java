d8f85b9c0bda2dc6401fbbba07eae6a79e90dc10
Marcin Miś <marcinxmis@gmail.com>
2019-04-15 23:19:09 +0200

    fixed css not loading bug

--- src/main/java/com/fis/is/terminy/configuration/SecurityConfiguration.java
+++ src/main/java/com/fis/is/terminy/configuration/SecurityConfiguration.java
@@ -67,7 +67,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
+        web.ignoring().antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**", "resources/static/**");
     }
 
 }
