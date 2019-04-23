bbe220040f79bb4188bc0fa2d5518ade4fd86e74
DorianKossowski <doriancfc@gmail.com>
2019-04-23 19:58:59 +0200

    added proper MySQL data

--- admin_site/config.php
+++ admin_site/config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_SERVER', 'localhost:3306');
-define('DB_USERNAME', 'tutorial');
-define('DB_PASSWORD', 'Passw0rd');
-define('DB_NAME', 'mojabaza');
+define('DB_SERVER', 'remotemysql.com:3306');
+define('DB_USERNAME', 'IoY9jiKVdl');
+define('DB_PASSWORD', 'Xz2uf258MJ');
+define('DB_NAME', 'IoY9jiKVdl');
  
 try {
 	$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
@@ -15,5 +15,5 @@ if($link === false){
     die("Could not connect with DB. " . mysqli_connect_error());
 }
 
-$db_name = 'mojabaza';
+$db_name = 'IoY9jiKVdl';
 ?>
