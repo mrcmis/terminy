4e4b91a930897ec41c2a8205e9f85889651b92b0
Airacobra <piotrek.m53@gmail.com>
2019-04-27 13:01:03 +0200

    Added AuthenticationSuccessHandler

--- src/main/java/com/fis/is/terminy/configuration/SecurityConfiguration.java
+++ src/main/java/com/fis/is/terminy/configuration/SecurityConfiguration.java
@@ -1,9 +1,10 @@
 package com.fis.is.terminy.configuration;
 
-
+import com.fis.is.terminy.controllers.MyUrlAuthenticationSuccessHandler;
 import com.fis.is.terminy.services.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.sql.DataSource;
@@ -40,6 +42,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(customUserDetailsService).passwordEncoder(encoder);
     }
 
+    @Bean
+    public AuthenticationSuccessHandler myAuthenticationSuccessHandler(){
+        return new MyUrlAuthenticationSuccessHandler();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
@@ -57,7 +64,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .csrf().disable().formLogin()
                 .loginPage("/login")
                 .failureUrl("/login?error=true")
-                .defaultSuccessUrl("/homeUser")
+                .successHandler(myAuthenticationSuccessHandler())
                 .usernameParameter("login")
                 .passwordParameter("password")
                 .and()
--- src/main/java/com/fis/is/terminy/controllers/AuthenticationController.java
+++ src/main/java/com/fis/is/terminy/controllers/AuthenticationController.java
@@ -41,7 +41,4 @@ public class AuthenticationController {
 
         return "login";
     }
-
-
-
 }
