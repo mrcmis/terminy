9af217feed0f4d12f5286e4804392b7cc5008fbc
DK <doriancfc@gmail.com>
2019-04-29 18:57:06 +0200

    added reading services from DB

--- src/main/java/com/fis/is/terminy/controllers/CompanyServicesController.java
+++ src/main/java/com/fis/is/terminy/controllers/CompanyServicesController.java
@@ -4,12 +4,14 @@ import com.fis.is.terminy.models.Company;
 import com.fis.is.terminy.models.CompanyService;
 import com.fis.is.terminy.repositories.CompanyServiceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
 
@@ -19,9 +21,11 @@ public class CompanyServicesController {
     private CompanyServiceRepository companyServiceRepository;
 
     @GetMapping("homeCompany/companyServices")
-    public String printServices(@Valid CompanyService companyService, BindingResult bindingResult,Model model){
-
-        return "companyServices";
+    public ModelAndView printServices(@Valid CompanyService companyService, BindingResult bindingResult, Pageable pageable){
+        Company currentCompany = (Company) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        ModelAndView mav = new ModelAndView("companyServices");
+        mav.addObject("servicesList", companyServiceRepository.findByCompanyId(currentCompany.getId(), pageable).getContent());
+        return mav;
     }
 
     @PostMapping("homeCompany/companyServices")
@@ -30,7 +34,7 @@ public class CompanyServicesController {
         companyService.setCompany(currentCompany);
         companyServiceRepository.save(companyService);
 
-        return "companyServices";
+        return "redirect:/homeCompany/companyServices";
     }
 
 }
